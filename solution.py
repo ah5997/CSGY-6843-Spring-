a50a0ac7a4db011b7345657e3fbb26331ca2323f
ArzooHussian <ah5997@nyu.edu>
2022-02-05 08:55:34 -0500

    Week one Attempt 3

--- solution.py
+++ solution.py
@@ -5,11 +5,7 @@
 def welcome_assignment_answers(question):
     #Students do not have to follow the skeleton for this assignment.
     #Another way to implement is using a "case" statements similar to C.
-   # if question == "Are encoding and encryption the same? - Yes/No":
-    #    answer = "The student should type the answer here"
-   # elif question == "Is it possible to decrypt a message without a key? - Yes/No":
-     #   answer = "The student should type the answer here"
-
+  
     if question == "In Slack, what is the secret passphrase posted in the #cyberfellows-computernetworking-fall2021 channel posted by a TA?":
         answer = "mTLS"
     elif question == "Are encoding and encryption the same? - Yes/No":
