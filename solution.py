3fed9b676c67c9500eef90995918566e5e055f48
ArzooHussian <ah5997@nyu.edu>
2022-02-05 09:16:48 -0500

    Week one Attempt 6

--- solution.py
+++ solution.py
@@ -1,4 +1,10 @@
+### welcome_assignment_answers
+### Input - All eight questions given in the assignment.
+### Output - The right answer for the specific question.
+
 def welcome_assignment_answers(question):
+    #The student doesn't have to follow the skeleton for this assignment.
+    #Another way to implement is using a "case" statements similar to C.
 
     if question == "In Slack, what is the secret passphrase posted in the #cyberfellows-computernetworking-fall2021 channel posted by a TA?":
         answer = "mTLS"
@@ -18,12 +24,11 @@ def welcome_assignment_answers(question):
         answer = 5
     elif question == "What layer of the TCP/IP model the protocol TCP belongs to? - The answer should be a numeric number":
         answer = 4
-    return(answer)
+    return (answer)
 # Complete all the questions.
 
 
 if __name__ == "__main__":
-    #use this space to debug and verify that the program works
+    # use this space to debug and verify that the program works
     debug_question = "Are encoding and encryption the same? - Yes/No"
     print(welcome_assignment_answers(debug_question))
-\
