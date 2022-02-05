825fc38ccfadff8e40a3a38775291d21111e48df
ArzooHussian <ah5997@nyu.edu>
2022-02-05 10:58:52 -0500

    Week one Attempt 13

--- solution.py
+++ solution.py
@@ -23,11 +23,5 @@ def welcome_assignment_answers(question):
         answer = 5
     elif question == "What layer of the TCP/IP model the protocol TCP belongs to? - The answer should be a numeric number":
         answer = 4
-    return(answer)
+    return answer
 # Complete all the questions.
-
-
-if __name__ == "__main__":
-    # use this space to debug and verify that the program works
-    debug_question = "Are encoding and encryption the same? - Yes/No"
-    print(welcome_assignment_answers(debug_question))
